994f4954d02eb99bdb1de2a423e2d83473476d6c
jwarner22 <joseph_warner@comcast.net>
2021-08-08 12:18:06 -0700

    'fixed modules theme reference'

--- src/components/PostLogin/CoursesBody/CourseTile/Data.js
+++ src/components/PostLogin/CoursesBody/CourseTile/Data.js
@@ -3,7 +3,7 @@
 
 export const AllEndgames = [
     {
-        id: 4,
+        id: 0,
         category: 'puzzle',
         type: 'endgame',
         headline: 'Endgame Shuffle',
@@ -15,338 +15,380 @@ export const AllEndgames = [
 
 export const Modules = [
     {
-        id: 4,
+        id: 1,
         category: 'puzzle',
         type: 'endgame',
+        type_ref: 'endgame',
         headline: 'Endgame Shuffle',
         subheading: 'A mixture of all endgame strategies.',
         description: 'Lorem ipsum mah brudda',
         img: require("../../../../Images/Endgame_shuffle.svg").default
     },{
-        id: 1, // `eventually use random str as modules will be dynamic for each user and may change over time
+        id: 2, // `eventually use random str as modules will be dynamic for each user and may change over time
         category: 'puzzle',
         type: 'endgame',
+        type_ref: 'bishopEndgame',
         headline: 'Bishop Endgame',
         subheading: 'Checkmate with a bishop.',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         img: require("../../../../Images/LondonOpeningIcon.svg").default
     },
     {
-        id: 2,
+        id: 3,
         category: 'puzzle',
         type: 'endgame',
+        type_ref: 'knightEndgame',
         headline: 'Knight Endgame',
         subheading: 'Practice the endgame with a knight.',
         description: 'Lorem ipsum mah brudda',
         img: require("../../../../Images/Pins.svg").default
     },{
-        id: 3,
+        id: 4,
         category: 'puzzle',
         type: 'endgame',
         headline: 'Pawn Endgame',
+        type_ref: 'pawnEndgame',
         subheading: `Capitalize with a pawn.`,
         description: 'Lorem ipsum mah brudda',
         img: require("../../../../Images/Skewer90px.svg").default
     },{
-        id: 4,
+        id: 5,
         category: 'puzzle',
         type: 'endgame',
+        type_ref: 'queenEndgame',
         headline: 'Queen Endgame',
         subheading: 'Practice using the queen in the endgame.',
         description: 'Lorem ipsum mah brudda',
         img: require("../../../../Images/Checkmate.svg").default
     },{
-        id: 5,
+        id: 6,
         category: 'puzzle',
         type: 'endgame',
+        type_ref: 'rookEndgame',
         headline: 'Rook Endgame',
         subheading: 'Practice using the rook in the endgame.',
         description: 'Lorem ipsum mah brudda',
         img: ''
     },{
-        id: 6,
+        id: 7,
         category: 'puzzle',
         type: 'endgame',
+        type_ref: 'queenRookEndgame',
         headline: 'Queen and Rook Endgame',
         subheading: 'The power couple',
         description: 'Lorem ipsum mah brudda',
         img: ''
     },{
-        id: 4,
+        id: 8,
         category: 'puzzle',
         type: 'endgame',
+        type_ref: 'zugzwang',
         headline: 'Zugzwang',
         subheading: `A tough road ahead.`,
         description: 'Lorem ipsum mah brudda',
         img: require("../../../../Images/Zugzwang.svg").default
     },{
-        id: 4,
+        id: 9,
         category: 'puzzle',
         type: 'checkmate',
+        type_ref: 'anastasiaMate',
         headline: `Anastasia's Mate`,
         subheading: 'Beautiful checkmates with a rook and knight.',
         description: 'Lorem ipsum mah brudda',
         img: ''
     },{
-        id: 4,
+        id: 10,
         category: 'puzzle',
         type: 'checkmate',
+        type_ref: 'arabianMate',
         headline: 'Arabian Mate',
         subheading: 'One of the oldest checkmate patterns.',
         description: 'Lorem ipsum mah brudda',
         img: ''
     },{
-        id: 4,
+        id: 11,
         category: 'puzzle',
         type: 'checkmate',
+        type_ref: 'bodensMate',
         headline: `Boden's Mate`,
         subheading: 'Criss-cross',
         description: 'Lorem ipsum mah brudda',
         img: require("../../../../Images/BodensMate.svg").default
     },{
-        id: 4,
+        id: 12,
         category: 'puzzle',
         type: 'checkmate',
+        type_ref: 'doubleBishopMate',
         headline: 'Double Bishop Mate',
         subheading: 'One bishop, two bishop, mate.',
         description: 'Lorem ipsum mah brudda',
         img: ''
     },{
-        id: 4,
+        id: 13,
         category: 'puzzle',
         type: 'checkmate',
+        type_ref: 'dovetailMate',
         headline: 'Dovetail Mate',
         subheading: 'Trap the black king.',
         description: 'Lorem ipsum mah brudda',
         img: require("../../../../Images/Dove.svg").default
     },{
-        id: 4,
+        id: 14,
         category: 'puzzle',
         type: 'checkmate',
+        type_ref: 'hookMate',
         headline: 'Hook Mate',
         subheading: 'Watch your back.',
         description: 'Lorem ipsum mah brudda',
         img: require("../../../../Images/Hook.svg").default
     },{
-        id: 4,
+        id: 15,
         category: 'puzzle',
         type: 'checkmate',
+        type_ref: 'mate',
         headline: 'Checkmate Shuffle',
         subheading: 'A mixture of checkmate strategies',
         description: 'Lorem ipsum mah brudda',
         img: require("../../../../Images/Endgame_shuffle.svg").default
     },{
-        id: 4,
+        id: 16,
         category: 'puzzle',
         type: 'checkmate',
+        type_ref: 'smotheredMate',
         headline: 'Smothered Mate',
         subheading: `I....can't....breath....`,
         description: 'Lorem ipsum mah brudda',
         img: require("../../../../Images/SmotheredMate.svg").default
     },{
-        id: 4,
+        id: 17,
         category: 'puzzle',
         type: 'midgame',
+        type_ref: 'advancedPawn',
         headline: 'Advanced Pawn',
         subheading: `We're almost home free!`,
         description: 'Lorem ipsum mah brudda',
         img: require("../../../../Images/AdvancedPawn.svg").default
     },{
-        id: 4,
+        id: 18,
         category: 'puzzle',
         type: 'midgame',
+        type_ref: 'attackingF2F7',
         headline: 'Attack f2f7',
         subheading: 'Capitalize on the weakness.',
         description: 'Lorem ipsum mah brudda',
         img: require("../../../../Images/f2f7.svg").default
     },{
-        id: 4,
+        id: 19,
         category: 'puzzle',
         type: 'midgame',
+        type_ref: 'attraction',
         headline: 'Attraction',
         subheading: `You've been bamboozled.`,
         description: 'Lorem ipsum mah brudda',
         img: require("../../../../Images/Attraction.svg").default
     },{
-        id: 4,
+        id: 20,
         category: 'puzzle',
         type: 'midgame',
+        type_ref: 'backRankMate',
         headline: 'Back Rank Mate',
         subheading: 'Watch out behind you!',
         description: 'Lorem ipsum mah brudda',
         img:require("../../../../Images/BackRank.svg").default
     },{
-        id: 4,
+        id: 21,
         category: 'puzzle',
         type: 'midgame',
+        type_ref: 'capturingDefender',
         headline: 'Capturing Defender',
         subheading: 'Improve your position by weakening your opponents defenses.',
         description: 'Lorem ipsum mah brudda',
         img: ''
     },{
-        id: 4,
+        id: 22,
         category: 'puzzle',
         type: 'midgame',
+        type_ref: 'clearance',
         headline: 'Clearance',
         subheading: `There's a line here, blow it open.`,
         description: 'Lorem ipsum mah brudda',
         img: require("../../../../Images/Clearance.svg").default
     },{
-        id: 4,
+        id: 23,
         category: 'puzzle',
         type: 'midgame',
+        type_ref: 'coercion',
         headline: 'Coercion',
         subheading: `Force one of your opponent's pieces into a vulnerable position.`,
         description: 'Lorem ipsum mah brudda',
         img: require("../../../../Images/Coercion.svg").default
     },{
-        id: 4,
+        id: 24,
         category: 'puzzle',
         type: 'midgame',
+        type_ref: 'defensiveMove',
         headline: 'Defensive Move',
         subheading: `You're under attack, protect yourself.`,
         description: 'Lorem ipsum mah brudda',
         img: ''
     },{
-        id: 4,
+        id: 25,
         category: 'puzzle',
         type: 'midgame',
+        type_ref: 'discoveredAttack',
         headline: 'Discoverd Attack',
         subheading: 'Surprise!',
         description: 'Lorem ipsum mah brudda',
         img: require("../../../../Images/DiscoveredAttack.svg").default
     },{
-        id: 4,
+        id: 26,
         category: 'puzzle',
         type: 'midgame',
+        type_ref: 'deflection',
         headline: 'Deflection',
         subheading: 'Cause a distraction.',
         description: 'Lorem ipsum mah brudda',
         img: require("../../../../Images/Deflection.svg").default
     },{
-        id: 4,
+        id: 27,
         category: 'puzzle',
         type: 'midgame',
+        type_ref: 'doubleCheck',
         headline: 'Double Check',
         subheading: 'Check x2',
         description: 'Lorem ipsum mah brudda',
         img: ''
     },{
-        id: 4,
+        id: 28,
         category: 'puzzle',
         type: 'midgame',
+        type_ref: 'enPassant',
         headline: 'En Passant',
         subheading: `Cature the pawn in passing.`,
         description: 'Lorem ipsum mah brudda',
         img: require("../../../../Images/EnPassant.svg").default
     },{
-        id: 4,
+        id: 29,
         category: 'puzzle',
         type: 'midgame',
+        type_ref: 'exposedKing',
         headline: 'Exposed King',
         subheading: 'The king is vulnerable, capitalize.',
         description: 'Lorem ipsum mah brudda',
         img: require("../../../../Images/ExposedKing.svg").default
     },{
-        id: 4,
+        id: 30,
         category: 'puzzle',
         type: 'midgame',
+        type_ref: 'fork',
         headline: 'Fork',
         subheading: `It's one or the other.`,
         description: 'Lorem ipsum mah brudda',
         img: ''
     },{
-        id: 4,
+        id: 31,
         category: 'puzzle',
         type: 'midgame',
+        type_ref: 'hangingPiece',
         headline: 'Hanging Piece',
         subheading: 'Find and capture the free material.',
         description: 'Lorem ipsum mah brudda',
         img: ''
     },{
-        id: 4,
+        id: 32,
         category: 'puzzle',
         type: 'midgame',
+        type_ref: 'interference',
         headline: 'Interference',
         subheading: `Disrupt the harmony of the opponent's position`,
         description: 'Lorem ipsum mah brudda',
         img: require("../../../../Images/Interference.svg").default
     },{
-        id: 4,
+        id: 33,
         category: 'puzzle',
         type: 'midgame',
+        type_ref: 'intermezzo',
         headline: 'Intermezzo',
         subheading: `The best move may not be so obvious, focus on development.`,
         description: 'Lorem ipsum mah brudda',
         img: require("../../../../Images/Intermezzo.svg").default
     },{
-        id: 4,
+        id: 34,
         category: 'puzzle',
         type: 'midgame',
+        type_ref: 'kingsideAttack',
         headline: 'Kingside Attack',
         subheading: `Attack the kingside.`,
         description: 'Lorem ipsum mah brudda',
         img: ''
     },{
-        id: 4,
+        id: 35,
         category: 'puzzle',
         type: 'midgame',
+        type_ref: 'overloading',
         headline: 'Overloading',
         subheading: `Your opponent can only do so many things at once.`,
         description: 'Lorem ipsum mah brudda',
         img: ''
     },{
-        id: 4,
+        id: 36,
         category: 'puzzle',
         type: 'midgame',
+        type_ref: 'pin',
         headline: 'Pin',
         subheading: `I'm pinned!`,
         description: 'Lorem ipsum mah brudda',
         img: require("../../../../Images/Pins.svg").default
     },{
-        id: 4,
+        id: 37,
         category: 'puzzle',
         type: 'midgame',
+        type_ref: 'promotion',
         headline: 'Promotion',
         subheading: `I deserve a promotion.`,
         description: 'Lorem ipsum mah brudda',
         img: require("../../../../Images/Promotion.svg").default
     },{
-        id: 4,
+        id: 38,
         category: 'puzzle',
         type: 'midgame',
+        type_ref: 'queensideAttack',
         headline: 'Queenside attack',
         subheading: `Attack the queenside`,
         description: 'Lorem ipsum mah brudda',
         img: ''
     },{
-        id: 4,
+        id: 39,
         category: 'puzzle',
         type: 'midgame',
+        type_ref: 'queitMove',
         headline: 'Quiet Move',
         subheading: `Focus on improving your position.`,
         description: 'Lorem ipsum mah brudda',
         img: require("../../../../Images/QuietMove.svg").default
     },{
-        id: 4,
+        id: 40,
         category: 'puzzle',
         type: 'midgame',
+        type_ref: 'sacrifice',
         headline: 'Sacrifice',
         subheading: `RIP`,
         description: 'Lorem ipsum mah brudda',
         img: ''
     },{
-        id: 4,
+        id: 41,
         category: 'puzzle',
         type: 'midgame',
+        type_ref: 'simplification',
         headline: 'Simplification',
         subheading: `"If winning, clarify; if losing, complicate."`,
         description: 'Lorem ipsum mah brudda',
         img: require("../../../../Images/Simplification.svg").default
     },{
-        id: 4,
+        id: 42,
         category: 'puzzle',
         type: 'midgame',
+        type_ref: 'skewer',
         headline: 'Skewer',
         subheading: `Line em' up, and knock em' down.`,
         description: 'Lorem ipsum mah brudda',
@@ -355,30 +397,34 @@ export const Modules = [
         id: 4,
         category: 'puzzle',
         type: 'midgame',
+        type_ref: 'trappedPiece',
         headline: 'Trapped Piece',
         subheading: `There is no escape.`,
         description: 'Lorem ipsum mah brudda',
         img: ''
     },{
-        id: 4,
+        id: 43,
         category: 'puzzle',
         type: 'midgame',
+        type_ref: 'underPromotion',
         headline: 'Under Promotion',
         subheading: `A queen may not be the best choice.`,
         description: 'Lorem ipsum mah brudda',
         img: ''
     },{
-        id: 4,
+        id: 44,
         category: 'puzzle',
         type: 'midgame',
+        type_ref: 'xRayAttack',
         headline: 'X-Ray Attack',
         subheading: `A level 2 skewer.`,
         description: 'Lorem ipsum mah brudda',
         img: ''
     },{
-        id: 4,
+        id: 45,
         category: 'puzzle',
         type: 'earlygame',
+        type_ref: 'castling',
         headline: 'Castling',
         subheading: `Protect the king!`,
         description: 'Lorem ipsum mah brudda',
--- src/components/PostLogin/Module.js
+++ src/components/PostLogin/Module.js
@@ -2,7 +2,7 @@ import Puzzle from '../Puzzle/puzzle.js';
 
 export default function Module(props) {
     //const text = props.location.state.text;
-    const theme = props.location.state.module.type;
+    const theme = props.location.state.module.type_ref;
     const id = props.location.state.module.type.id;
     console.log(props)
     //console.log(text)
