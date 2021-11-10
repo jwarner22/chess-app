30f25f925bca0c264b1d86d2cffeb24e1b403c2f
Caleb Gibson <calebgibson21@gmail.com>
2021-11-09 21:38:55 -0600

    White and black move indication done

--- src/components/Puzzle/PuzzleComponents/TurnIndicator/BlackIndicator.js
+++ src/components/Puzzle/PuzzleComponents/TurnIndicator/BlackIndicator.js
@@ -1,11 +1,27 @@
 import React from 'react'
+import styled from "styled-components"
 
 const BlackIndicator = () => {
     return (
-        <div>
+        <BlackIndicatorContainer>
+            <BlackIndicatorText>
             Black to move
-        </div>
+            </BlackIndicatorText>
+        </BlackIndicatorContainer>
     )
 }
 
 export default BlackIndicator
+
+const BlackIndicatorContainer = styled.div`
+    padding: 8px;
+    display: flex;
+    background: #000;
+    border-radius: 5px;
+    margin-top: 12px;
+`
+
+const BlackIndicatorText = styled.span`
+    color: #fff;
+    font-weight: 600;
+`
--- src/components/Puzzle/PuzzleComponents/TurnIndicator/WhiteIndicator.js
+++ src/components/Puzzle/PuzzleComponents/TurnIndicator/WhiteIndicator.js
@@ -1,11 +1,28 @@
 import React from 'react'
+import styled from "styled-components"
 
 const WhiteIndicator = () => {
     return (
-        <div>
+        <WhiteIndicatorContainer>
+            <WhiteIndicatorText>
             White to move
-        </div>
+            </WhiteIndicatorText>
+        </WhiteIndicatorContainer>
     )
 }
 
 export default WhiteIndicator
+
+const WhiteIndicatorContainer = styled.div`
+    padding: 8px;
+    display: flex;
+    background: #fff;
+    border-radius: 5px;
+    margin-top: 12px;
+    border: 1px solid #000;
+`
+
+const WhiteIndicatorText = styled.span`
+    color: #000;
+    font-weight: 600;
+`
