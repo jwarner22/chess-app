cb570263ce3c20ffd99874be59deac11a0940395
jwarner22 <joseph_warner@comcast.net>
2021-11-25 12:54:30 -0800

    exit modal

--- src/components/BackButton.js
+++ src/components/BackButton.js
@@ -2,14 +2,30 @@ import React from 'react'
 import styled from "styled-components"
 import {useHistory} from "react-router-dom"
 import { GrFormClose } from "react-icons/gr";
+import swal from 'sweetalert';
 
 const BackButton = () => {
     let history = useHistory();
 
+    const handleClick = () => { 
+        swal({
+            title: "Are you sure?",
+            text: "All progress will be lost.",
+            icon: "warning",
+            dangerMode: true,
+            buttons: true
+          })
+          .then(willDelete => {
+            if (willDelete) {
+                history.goBack()
+            }
+          });
+    }
+
     return (
         <div>
             <ClickBack>
-           <GrFormClose size={32} onClick={() => history.goBack()}>Back</GrFormClose> 
+           <GrFormClose size={32} onClick={() => handleClick()}>Back</GrFormClose> 
            </ClickBack>
         </div>
     )
@@ -21,3 +37,4 @@ const ClickBack = styled.div`
     display: flex;
     cursor: pointer;
 `
+//onClick={() => history.goBack()}
