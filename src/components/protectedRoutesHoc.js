c0f442163d02888fa8541641402f8081e8411c8c
jwarner22 <joseph_warner@comcast.net>
2022-01-11 18:40:11 -0800

    removed firebase event for screen view

--- src/components/protectedRoutesHoc.js
+++ src/components/protectedRoutesHoc.js
@@ -2,17 +2,15 @@ import React, {useContext, useEffect} from 'react';
 import { Route, Redirect, withRouter } from 'react-router-dom';
 import { bool, any, object } from 'prop-types';
 import {AuthContext} from './Auth.js';
-import {getAnalytics, logEvent} from "firebase/analytics";
 
 //This component checks to see if the user is logged in or not. 
 const ProtectedRouteHoc = ({ component: Component, ...rest }) => {
 	//If the user is authenticated, let them pass. If not, redirect them. 
-	const analytics = getAnalytics();
+
 	const {currentUser} = useContext(AuthContext)
 	const {path} = rest;
 
 	if (currentUser || rest.public) {
-		logEvent(analytics, 'screen_view', {'firebase_screen': path.substring(1)});
 		return (
 			<Route
 				{...rest}
