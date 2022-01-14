7dc24ccc22a15b28dd80a63471e585f7ee0f4b61
jwarner22 <joseph_warner@comcast.net>
2022-01-13 18:16:14 -0800

    mixed module

--- backend/read_puzzles.py
+++ backend/read_puzzles.py
@@ -10,11 +10,21 @@ def get_puzzle(db: Session, puzzle_id: str):
 def get_puzzles(db: Session, rating: int = 1500, theme: str = '', limit: int = 30):
     upperBound = rating + 50
     lowerBound = max(1000, (rating - 50))
-    puzzles = db.query(models.Puzzles).filter((models.Puzzles.rating > lowerBound) & (models.Puzzles.rating < upperBound) & (models.Puzzles.themes.contains(theme))).order_by(func.random()).limit(limit).all() # .first() # .limit(10)
+    
+    # pull random puzzles from the database based on theme and rating
+    if theme != 'mix':
+        puzzles = db.query(models.Puzzles).filter((models.Puzzles.rating > lowerBound) & (models.Puzzles.rating < upperBound) & (models.Puzzles.themes.contains(theme))).order_by(func.random()).limit(limit).all() # .first() # .limit(10)
+    else: # healthy mix of puzzles from all themes and categories for a specific rating
+        puzzles = db.query(models.Puzzles).filter((models.Puzzles.rating > lowerBound) & (models.Puzzles.rating < upperBound)).order_by(func.random()).limit(limit).all()
+    
     while (upperBound < 2500 and len(puzzles) == 0):
         upperBound += 50
         lowerBound = max(1000, (lowerBound - 50))
-        puzzles = db.query(models.Puzzles).filter((models.Puzzles.rating > lowerBound) & (models.Puzzles.rating < upperBound) & (models.Puzzles.themes.contains(theme)) & sqlalchemy.not_(models.Puzzles.themes.contains('promotion'))).order_by(func.random()).limit(limit).all() # .first() # .limit(10)
+        
+        if theme != 'mix':
+            puzzles = db.query(models.Puzzles).filter((models.Puzzles.rating > lowerBound) & (models.Puzzles.rating < upperBound) & (models.Puzzles.themes.contains(theme))).order_by(func.random()).limit(limit).all() # .first() # .limit(10)
+        else:
+            puzzles = db.query(models.Puzzles).filter((models.Puzzles.rating > lowerBound) & (models.Puzzles.rating < upperBound)).order_by(func.random()).limit(limit).all()
     return puzzles
 
 
