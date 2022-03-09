4194703cf456ce2550be96371e66e7d73cfc8dad
Caleb Gibson <calebgibson21@gmail.com>
2022-03-09 10:52:39 -0600

    Opening tree useLayoutEffect

--- src/components/PostLogin/Views/Openings/OpeningsDashboardTest.js
+++ src/components/PostLogin/Views/Openings/OpeningsDashboardTest.js
@@ -1,4 +1,4 @@
-import {useLayoutEffect, useState} from 'react';
+import {useLayoutEffect, useEffect, useState} from 'react';
 import styled from 'styled-components'
 import {Link, useParams} from 'react-router-dom';
 import useFetch from '../../../../api/useFetch';
@@ -9,50 +9,54 @@ import { PageContainer } from '../../../UI_Kit/Page';
 import PreviousSelection from '../../../UI_Kit/PreviousSelection';
 import CategoryTitle from '../../../UI_Kit/CategoryTitle/CategoryTitle';
 import CurrentOpeningTreeTile from '../../../UI_Kit/Tiles/CurrentOpeningTreeTile';
+import Loader from '../../../Loader';
 
 const OpeningsDashboardTest = () => {
     const {get} = useFetch(baseURL);
     const [openingModules, setOpeningModules] = useState([]);
     const [currentOpening, SetCurrentOpening] = useState({});
     const {moves} = useParams();
+    const [loading, setLoading] = useState(true);
+
+    // useEffect(() => {
+    //     console.log('fetch new openings')
+    //     fetchOpenings();
+    // },[moves])
 
     useLayoutEffect(() => {
-        console.log('fetch new openings')
-        fetchOpenings();
-    },[moves])
-
-
-
-    const fetchOpenings = async () => {
-        // fetch data from opening-data endpoint
-        let endpoint = '/openings-data/'
-        let queryParams = `?moves=${moves}`
-        let openings = await get(endpoint+queryParams);
-        console.log({response: openings})
-        if (openings.length === 1) {
-            SetCurrentOpening(openings[0]); // if no variatins, set current opening to the opening
-            setOpeningModules([]); // set the opening modules to the variations
-            return;
-        }
-        let main = openings.filter(opening => opening.uci === moves)[0];
-        // check if nb plays exists for mainline
-        if (main != null) {
-            console.log(main.np_lichess)
-            if (openings.some(opening => opening.np_lichess == null)) {
-            console.log('no nb plays')
-            let endpoint = '/openings-data/lichess-explorer/'
-             openings = await get(endpoint+queryParams);  
+        const fetchOpenings = async () => {
+            // fetch data from opening-data endpoint
+            let endpoint = '/openings-data/'
+            let queryParams = `?moves=${moves}`
+            let openings = await get(endpoint+queryParams);
+            console.log({response: openings})
+            if (openings.length === 1) {
+                SetCurrentOpening(openings[0]); // if no variatins, set current opening to the opening
+                setOpeningModules([]); // set the opening modules to the variations
+                return;
             }
+            let main = openings.filter(opening => opening.uci === moves)[0];
+            // check if nb plays exists for mainline
+            if (main != null) {
+                console.log(main.np_lichess)
+                if (openings.some(opening => opening.np_lichess == null)) {
+                console.log('no nb plays')
+                let endpoint = '/openings-data/lichess-explorer/'
+                 openings = await get(endpoint+queryParams);  
+                }
+            }
+            openings = openings.filter(opening => opening.np_lichess != null) // filter out openings with no nb plays
+            openings = openings.sort((a,b) => b.np_lichess - a.np_lichess) // sort by nb plays
+            openings = openings.slice(1,6) //choose a max of 5 openings
+    
+            let selectedOpening = openings[0];
+            setOpeningModules(openings.slice(1,6));
+            SetCurrentOpening(selectedOpening)
+            setLoading(false)
         }
-        openings = openings.filter(opening => opening.np_lichess != null) // filter out openings with no nb plays
-        openings = openings.sort((a,b) => b.np_lichess - a.np_lichess) // sort by nb plays
-        openings = openings.slice(1,6) //choose a max of 5 openings
+        fetchOpenings()
+    }, [moves])
 
-        let selectedOpening = openings[0];
-        setOpeningModules(openings.slice(1,6));
-        SetCurrentOpening(selectedOpening)
-
-    }
     console.log({currentOpening: currentOpening})
 
     return(
@@ -60,6 +64,8 @@ const OpeningsDashboardTest = () => {
         <PageContainer>
         <CategoryTitle>Opening Mastery Tree</CategoryTitle>
         <PreviousSelection />
+        {loading ? (  <Loader/>) : (
+            <>
         <CurrentOpeningTreeTile moves={currentOpening.uci} name={currentOpening.name} popularity={currentOpening.np_lichess}/>
         {(openingModules.length > 0) && 
 
@@ -72,7 +78,9 @@ const OpeningsDashboardTest = () => {
                 </OpeningLink>
             )})}
         </PuzzleTileGrid>     
-        }
+         }
+        </>
+        )}
         </PageContainer>
         </>
     )
