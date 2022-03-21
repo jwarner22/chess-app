7ae83662456a1e0757bbc76505707c3033f40c4c
jwarner22 <joseph_warner@comcast.net>
2022-03-21 07:48:19 -0700

    daily tile fix

--- src/components/PostLogin/Views/DailyPuzzle/DailyPuzzle.js
+++ src/components/PostLogin/Views/DailyPuzzle/DailyPuzzle.js
@@ -200,6 +200,7 @@ export default function DailyPuzzzle(props) {
            if (index===3) { // if module is an opening module
              //{pathname: `/pre-opening-test/${props.currentOpening.uci}`
              let uci = openingStats.find(entry => entry.opening_id === module.theme_id).uci
+             module = {...module, headline: module.title, subheading: 'Opening', img: require('../../../../Images/Books.svg').default };
              return(
               <Link key={index} style={{textDecoration: 'none'}} to={module.locked ? '#' : {pathname: `/pre-opening-test/${uci}`, state: {module: module, isDaily: true, location: module.location}}}>
               <DailyPuzzleModuleContainer key={index} {...module} />
