374a8f7eb4982d6546832bb9ef96caefb8c096f8
Caleb Gibson <calebgibson21@gmail.com>
2022-03-24 11:43:42 -0500

    python pull

--- src/Pages/Openings/OpeningMastery.js
+++ src/Pages/Openings/OpeningMastery.js
@@ -1,9 +1,17 @@
-import styled from 'styled-components'
-import LargeConatiner from '../../components/UI_Kit/Boxes/Containers/LargeConatiner'
-import CategoryTitle from '../../components/UI_Kit/CategoryTitle/CategoryTitle'
-import MasteryTiles from './MasteryTiles'
+import styled from 'styled-components';
+import { useContext } from 'react';
+import LargeConatiner from '../../components/UI_Kit/Boxes/Containers/LargeConatiner';
+import CategoryTitle from '../../components/UI_Kit/CategoryTitle/CategoryTitle';
+import MasteryTiles from './MasteryTiles';
+
+import { UserContext } from '../../providers/GlobalState';
 
 const OpeningMastery = () => {
+
+const [openingStats] = useContext(UserContext);
+
+console.log(openingStats)
+
   return (<>
         <CategoryTitle>OpeningMastery</CategoryTitle>
           <LargeConatiner>
